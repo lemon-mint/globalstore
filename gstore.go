de8a99689d3a365c25539f5cf344249fcf47003b
lemon-mint <ice.lemons.mint@gmail.com>
2022-02-05 21:50:45 +0900

    CAS, D

--- gstore.go
+++ gstore.go
@@ -1,6 +1,7 @@
 package globalstore
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/base32"
 	"os"
@@ -81,6 +82,75 @@ func (gs *GlobalStore) Set(key string, value []byte) error {
 	return nil
 }
 
+func (gs *GlobalStore) Delete(key string) error {
+	err := gs.fl.Lock()
+	if err != nil {
+		return err
+	}
+	defer gs.fl.Unlock()
+
+	db, err := bbolt.Open(gs.filepath, 0666, nil)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	err = db.Update(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte("data"))
+		if b == nil {
+			return nil
+		}
+		return b.Delete([]byte(key))
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (gs *GlobalStore) CompareAndSwap(key string, oldValue []byte, newValue []byte) (bool, error) {
+	err := gs.fl.Lock()
+	if err != nil {
+		return false, err
+	}
+	defer gs.fl.Unlock()
+
+	db, err := bbolt.Open(gs.filepath, 0666, nil)
+	if err != nil {
+		return false, err
+	}
+	defer db.Close()
+
+	err = db.Update(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte("data"))
+		if b == nil {
+			return nil
+		}
+		v := b.Get([]byte(key))
+		if v == nil {
+			return nil
+		}
+		if !bytes.Equal(v, oldValue) {
+			return nil
+		}
+		return b.Put([]byte(key), newValue)
+	})
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (gs *GlobalStore) Lock() error {
+	return gs.fl.Lock()
+}
+
+func (gs *GlobalStore) Unlock() error {
+	return gs.fl.Unlock()
+}
+
 var DefaultGlobalStore = Open("default")
 
 func G(key string) ([]byte, error) {
@@ -89,4 +159,20 @@ func G(key string) ([]byte, error) {
 
 func S(key string, value []byte) error {
 	return DefaultGlobalStore.Set(key, value)
+}
+
+func CAS(key string, oldValue []byte, newValue []byte) (bool, error) {
+	return DefaultGlobalStore.CompareAndSwap(key, oldValue, newValue)
+}
+
+func D(key string) error {
+	return DefaultGlobalStore.Delete(key)
+}
+
+func Lock() error {
+	return DefaultGlobalStore.Lock()
+}
+
+func Unlock() error {
+	return DefaultGlobalStore.Unlock()
 }
